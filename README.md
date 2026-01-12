cedef0ea453f9ad719da946e6692adbc391959dd
Rafael <jarevaloagu2@gmail.com>
2026-01-12 01:16:02 -0500

    Update

---
+++ README.md
@@ -0,0 +1,20 @@
+## Pixelio
+Web Builder (Test)
+Developer Rafael Aguirre
+
+## Install Dependencies
+```bash
+npm i
+yarn i
+```
+
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+# or
+bun dev
+```
+
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+import { DESTRUCTION } from "dns";
+
+const Page = () =>{
+  return(
+    <div>
+      <Button variant="destructive">
+         Clicck Me
+        </Button>
+    </div>
+  )
+}
+export default Page;
